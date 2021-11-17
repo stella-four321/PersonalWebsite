635631df8d928ed5fb8407e2cdec752ef15cae82
Stella Fournier <stefour321@gatech.edu>
2021-11-16 21:18:52 -0500

    add

---
+++ box.js
@@ -0,0 +1,22 @@
+let box = document.getElementById('box'),
+btn = document.querySelector('button');
+
+btn.addEventListener('click', function () {
+  
+  if (box.classList.contains('hidden')) {
+    box.classList.remove('hidden');
+    setTimeout(function () {
+      box.classList.remove('visuallyhidden');
+    }, 20);
+  } else {
+    box.classList.add('visuallyhidden');    
+    box.addEventListener('transitionend', function(e) {
+      box.classList.add('hidden');
+    }, {
+      capture: false,
+      once: true,
+      passive: false
+    });
+  }
+  
+}, false);
